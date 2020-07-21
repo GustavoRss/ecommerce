dc31673ff53619b60d680f547cdf0a7dd86ddbf4
Gotstaf <41273411+Gotstaf@users.noreply.github.com>
2020-07-21 10:41:06 -0300

    Carrinho Produtos

--- site.php
+++ site.php
@@ -84,6 +84,12 @@ $app -> get("/cart/:idproduct/add", function($idproduct){
 
     $cart = Cart::getFromSession();
 
+    $qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd']:1;
+
+    for($i = 1; $i < $qtd; $i++){
+        $cart->addProduct($product);
+    }
+
     $cart->addProduct($product);
 
     header("Location: /cart");
